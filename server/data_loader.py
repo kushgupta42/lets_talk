d87463b42b96701fb08a4c9354c2f90cb548e4d5
Kush Gupta <kushgupta42@gmail.com>
2023-10-12 17:37:58 +0530

    chant gpt integrated

--- server/data_loader.py
+++ server/data_loader.py
@@ -42,3 +42,54 @@ def write_subs_to_json(json_formatted_subs: json, folder_path: str, video_id: st
 
     with open(f"{folder_path}/{video_id}.json", mode="w", encoding="utf-8") as f:
         f.write(json_formatted_subs)
+
+
+def create_time_chunks(
+    input_directory: str, output_directory: str, time_chunk_size_mins: int
+):
+    # iterate over all the files in the directory:
+    for filename in os.listdir(input_directory):
+        file = os.path.join(input_directory, filename)
+        print(file)
+        f = open(file)
+        data = json.loads(f.read())
+
+        # # logging
+        # print(type(data))
+        # print(f"data: {data}")
+
+        # start with empty dict that you will keep adding to
+        timechunks = {}
+
+        # helper variables to keep track of position, timechunk size, and collating the text
+        current_time_pos = 0
+        running_composite = []
+        chunk_num = 1
+
+        # loop to create the chunks
+        #! the last chunk might be getting skipped
+        for chunk in data:
+            end_time = chunk["start"] + chunk["duration"]
+            print(f"endtime: {end_time}")
+            print(f"current_time_pos: {current_time_pos}")
+            if math.isclose(end_time, chunk_num * 60 * time_chunk_size_mins, abs_tol=5):
+                text = " ".join(running_composite)
+                timechunks[f"{current_time_pos} - {end_time}"] = text
+                running_composite.clear()
+                current_time_pos = end_time
+                chunk_num += 1
+            else:
+                running_composite.append(chunk["text"])
+
+        print(f"running_composite: {running_composite}")
+        print(f"time chunks: {timechunks}")
+
+        if len(running_composite):
+            text = " ".join(running_composite)
+            timechunks[f"{current_time_pos} - {end_time}"] = text
+
+        # write the timechunks to a json file
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+        with open(f"{output_directory}/{filename}", "w") as f:
+            json.dump(timechunks, f)
--- server/main.py
+++ server/main.py
@@ -17,8 +17,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-GPT_INDEX_FOLDER = "data/gpt_index"
-SUBS_FOLDER = "data/subs"
+system_prompt = """You are an agent that is trained to take information from the context and answer questions within that context by accepting a loosely related query. You are free to use an analogy of the question if the information is not verbatim in the context.
+
+If the context of the question is even loosely related to the content then provide a reasonably good answer.
+
+If the question is too outside the context of the video just respond back with 'PromptError'. Do not overuse 'PromptError'. Use sparingly.
+
+Give the above conditions, answer the below user query.
+
+"""
 
 
 @app.get("/")
@@ -34,12 +41,30 @@ async def get_response(video_id: str, question: str):
         f.write(subs)
         f.close()
         write_subs_to_json(subs, SUBS_FOLDER, video_id)
+        create_time_chunks("data/subs", "data/timechunks", time_chunk_size_mins)
+        data = json.load(open(file=f"data/timechunks/{video_id}.json", mode="r"))
+        # keys, text = list(zip(*data.items()))
+        # yaha se kuch samajh nahin aaya
+        nodes = [Node(text=text, doc_id=keys) for keys, text in data.items()]
+        index = GPTTreeIndex(nodes=nodes)
+        # save it
+        index.save_to_disk(f"data/gpt_index/{video_id}.json")
 
-        return {
-            "response": question,
-            "start_time": 0,
-            "end_time": 200,
-        }
+        response = index.query(f"{system_prompt}\n{question}")
+
+        print(f"response: {response}")
+        source = " ".join(
+            [node_with_score.node.doc_id for node_with_score in response.source_nodes]
+        )
+        print(f"sources: {source}")
+
+        # return {
+        #     "response": question,
+        #     "start_time": 0,
+        #     "end_time": 200,
+        # }
+
+        return {"response": str(response), "sources": source}
     except Exception as error:
         print(error)
         raise HTTPException(status_code=400, detail=str(error)) from error
